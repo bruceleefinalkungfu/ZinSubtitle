ab047cdf13ffb90618cfdc83a5404b6cabfeb40e
zin <zin@zin-PC>
2019-04-07 01:48:35 +0530

    bulk subtitle processing

--- src/zin/test/CLI.java
+++ src/zin/test/CLI.java
@@ -22,6 +22,7 @@ import zin.z.constant.ZinConstant;
 public class CLI {
 	static ZinFile file = new ZinFile();
 	static List<String> list = null;
+	static String separator = "\"";
 	public static String getFileData(String fileName) throws Exception {
 		String fData = file.getStringFromFile(fileName);
 		if(fileName.endsWith(".srt"))
@@ -31,12 +32,65 @@ public class CLI {
 		}
 		return fData;
 	}
+	
+	public static void createBackup(String files) throws Exception {
+		String [] fileNames = files.split(separator);
+		for(String fileName : fileNames)
+			file.write(ZinConstant.ZIN_DATA_DIR+"dumped_subs"+ZinConstant.SLASH+ fileName.substring(fileName.lastIndexOf("\\")+1)+"."+new Date().getTime(), getFileData(fileName));
+	}
+	
+	public static void processSub(String files, String fromTime, String subtract) throws Exception {
+		String [] fileNames = files.split(separator);
+		for(String fileName : fileNames) {
+			SubtitleUtil.subtractFromSrt(fileName, "0");
+			String fileFullData = getFileData(fileName).trim();
+			if(fileName.endsWith(".srt")) {
+				SubtitleUtil.subtractFromSrt(fileName, fromTime);
+				SubtitleUtil.addToSrt(fileName, fromTime);
+				String dataToBeSubtracted = getFileData(fileName).trim();
+				String dataToBeNotSubtracted = fileFullData.replaceAll("(\\Q"+dataToBeSubtracted+"\\E)", "");
+				SubtitleUtil.subtractFromSrt(fileName, subtract);
+				String subtractedData = getFileData(fileName);
+				String dataToWrite = dataToBeNotSubtracted + subtractedData;
+				file.write(fileName, dataToWrite);
+			}
+			else if(fileName.endsWith(".ass")) {
+				SubtitleUtil.subtractFromAss(fileName, subtract);
+				SubtitleUtil.addToAss(fileName, subtract);
+				String dataToBeSubtracted = getFileData(fileName).trim();
+				String dataToBeNotSubtracted = fileFullData.replaceAll("(\\Q"+dataToBeSubtracted+"\\E)", "");
+				SubtitleUtil.subtractFromAss(fileName, subtract);
+				String subtractedData = getFileData(fileName);
+				String dataToWrite = dataToBeNotSubtracted + subtractedData;
+				file.write(fileName, dataToWrite);
+			}
+			else
+			{
+				throw new Exception("File "+fileName+" is not a subtitle file");
+			}
+		}
+	}
+	
+	public static void subtractSub(String files, String subtract) throws Exception {
+		String [] fileNames = files.split(separator);
+		for(String fileName : fileNames) {
+			if(fileName.endsWith(".srt"))
+				SubtitleUtil.subtractFromSrt(fileName, subtract);
+			else if(fileName.endsWith(".ass"))
+				SubtitleUtil.subtractFromAss(fileName, subtract);
+			else
+			{
+				throw new Exception("File "+fileName+" is not a subtitle file");
+			}
+		}
+	}
+	
 	public static void main(String[] args) throws Exception {
 		outerMostLoop:
 		for(;;) {
 			String fileName = getFileName();
 			fileName = fileName.toLowerCase();
-			file.write(ZinConstant.ZIN_DATA_DIR+"dumped_subs"+ZinConstant.SLASH+ fileName.substring(fileName.lastIndexOf("\\")+1)+"."+new Date().getTime(), getFileData(fileName));
+			createBackup(fileName);
 			outerLoop:
 			for(;;) {
 				String subtract =  ZIO.input("How much to subtract? or press P to process or C to choose another file");
@@ -47,35 +101,9 @@ public class CLI {
 						subtract =  ZIO.input("How much to subtract? Press 'D' for when you're done and wanna begin subtraction");
 						if(subtract.equalsIgnoreCase("d")) {
 							for(Map.Entry<String, String> entry : processSubtitle.getEntries()) {
-								SubtitleUtil.subtractFromSrt(fileName, "0");
-								String fileFullData = getFileData(fileName).trim();
 								fromTime = entry.getKey();
 								subtract = entry.getValue();
-								if(fileName.endsWith(".srt")) {
-									SubtitleUtil.subtractFromSrt(fileName, fromTime);
-									SubtitleUtil.addToSrt(fileName, fromTime);
-									String dataToBeSubtracted = getFileData(fileName).trim();
-									String dataToBeNotSubtracted = fileFullData.replaceAll("(\\Q"+dataToBeSubtracted+"\\E)", "");
-									SubtitleUtil.subtractFromSrt(fileName, subtract);
-									String subtractedData = getFileData(fileName);
-									String dataToWrite = dataToBeNotSubtracted + subtractedData;
-									file.write(fileName, dataToWrite);
-								}
-								else if(fileName.endsWith(".ass")) {
-									SubtitleUtil.subtractFromAss(fileName, subtract);
-									SubtitleUtil.addToAss(fileName, subtract);
-									String dataToBeSubtracted = getFileData(fileName).trim();
-									String dataToBeNotSubtracted = fileFullData.replaceAll("(\\Q"+dataToBeSubtracted+"\\E)", "");
-									SubtitleUtil.subtractFromAss(fileName, subtract);
-									String subtractedData = getFileData(fileName);
-									String dataToWrite = dataToBeNotSubtracted + subtractedData;
-									file.write(fileName, dataToWrite);
-								}
-								else
-								{
-									System.out.println("File "+fileName+" is not a subtitle file");
-									break;
-								}	
+								processSub(fileName, fromTime, subtract);	
 							}
 							break outerLoop;
 						}
@@ -84,15 +112,7 @@ public class CLI {
 				} else if(subtract.equalsIgnoreCase("c")) {
 					break;
 				} else {
-					if(fileName.endsWith(".srt"))
-						SubtitleUtil.subtractFromSrt(fileName, subtract);
-					else if(fileName.endsWith(".ass"))
-						SubtitleUtil.subtractFromAss(fileName, subtract);
-					else
-					{
-						System.out.println("File "+fileName+" is not a subtitle file");
-						break;
-					}
+					subtractSub(fileName, subtract);
 				}
 			}
 		}
@@ -104,13 +124,16 @@ public class CLI {
 		for(int i=0 ; i<list.size() ; i++) {
 			System.out.println((i+1) +" => " + list.get(i).substring(list.get(i).lastIndexOf("\\")+1));
 		}
-		String fileName = ZIO.input("Enter file index/name or leave it blank if it to be treated like zin.srt");
+		String fileName = ZIO.input("Enter file index/name");
 		if(fileName == null || fileName.trim().isEmpty())
 			fileName = "zin.srt";
 		else if(fileName.length() <= 2)
 		{
 			int index = Integer.parseInt(fileName.trim());
 			fileName = list.get(index-1);
+		}
+		else if (fileName.equalsIgnoreCase("all") || fileName.equalsIgnoreCase("bulk")) {
+			fileName = getAllFileNames();
 		}
 		else {
 			File f = file.getFileFromFileName(fileName);
@@ -119,13 +142,16 @@ public class CLI {
 				for(int i=0 ; i<list.size() ; i++) {
 					System.out.println((i+1) +" => " + list.get(i).substring(list.get(i).lastIndexOf("\\")+1));
 				}
-				fileName = ZIO.input("Enter file index/name or leave it blank if it to be treated like zin.srt");
+				fileName = ZIO.input("Enter file index/name");
 				if(fileName == null || fileName.trim().isEmpty())
 					fileName = "zin.srt";
 				else if(fileName.length() <= 2)
 				{
 					int index = Integer.parseInt(fileName.trim());
 					fileName = list.get(index-1);
+				}
+				else if (fileName.equalsIgnoreCase("all") || fileName.equalsIgnoreCase("bulk")) {
+					fileName = getAllFileNames();
 				}
 				else if( ! file.isFilePathAbsolute(fileName))
 				{
@@ -134,6 +160,16 @@ public class CLI {
 			}
 		}
 		return fileName;
+	}
+	
+	public static String getAllFileNames() {
+		StringBuffer sf = new StringBuffer();
+		for(String file : list) {
+			if(! file.equalsIgnoreCase(list.get(0)))
+				sf.append(separator);
+			sf.append(file);
+		}
+		return new String(sf);
 	}
 	
 	public static List<String> getFiles(String dir) throws Exception {
